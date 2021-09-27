3a4ef2a7c06a8e35a773f80688156d44074e6bbb
MauricioHub <mauricio.guzmanjc@gmail.com>
2021-09-26 19:32:11 -0500

    first implement of RecyclerView component.

--- app/src/main/java/com/fomat/newsonline/MainActivity.kt
+++ app/src/main/java/com/fomat/newsonline/MainActivity.kt
@@ -2,10 +2,53 @@ package com.fomat.newsonline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var newRecyclerView: RecyclerView
+    private lateinit var newArrayList: ArrayList<News>
+    lateinit var imageId : Array<Int>
+    lateinit var heading : Array<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageId = arrayOf(
+            R.drawable.downloada,
+            R.drawable.downloadb,
+            R.drawable.imagesa,
+            R.drawable.imagesb,
+            R.drawable.imagesc,
+            R.drawable.imagesd,
+            R.drawable.imagese
+        )
+
+        heading = arrayOf(
+            "notice 1",
+            "notice 2",
+            "notice 3",
+            "notice 4",
+            "notice 5",
+            "notice 6",
+            "notice 7"
+        )
+
+        newRecyclerView = findViewById(R.id.recyclerView)
+        newRecyclerView.layoutManager = LinearLayoutManager(this)
+        newRecyclerView.setHasFixedSize(true)
+
+        newArrayList = arrayListOf<News>()
+        getUserData()
+    }
+
+    private fun getUserData() {
+        for (i in imageId.indices){
+            val news = News(imageId[i], heading[i])
+            newArrayList.add(news)
+        }
+        newRecyclerView.adapter = MyAdapter(newArrayList)
     }
 }
---
+++ app/src/main/java/com/fomat/newsonline/News.kt
@@ -0,0 +1,3 @@
+package com.fomat.newsonline
+
+data class News(var titleImage : Int, var heading : String)
